d58b35c9a3ce208c43bd690290aa173c8f20a4ac
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-05-08 09:05:10 +0200

    Proper randr.sh

--- randr.sh
+++ randr.sh
@@ -5,6 +5,5 @@ backlight-control -s 100
 xrandr --output VGA-0 --off --output DVI-0 --off --output LVDS --auto
 
 i3-nagbar -m "EBRND'S SUPER-COOL I3WM SCREEN CONFIG UTILITY" -t warning \
-    -b "LVDS + DVI"  "xrandr --output VGA-0 --off --output LVDS --auto --output DVI-0 --auto --right-of LVDS" \
-    -b "LVDS + VGA"  "xrandr --output DVI-0 --off --output LVDS --auto --output VGA-0 --auto --right-of LVDS" \
-    -b "VGA ONLY"    "xrandr --output LVDS --off --output DVI-0 --off --output VGA-0 --auto"
+    -b "HDMI + VGA"   "xrandr --output LVDS1 --off  --output HDMI1 --auto --output VGA1 --auto --right-of HDMI1" \
+    -b "Laptop only"  "xrandr --output LVDS1 --auto --output HDMI1 --off  --output VGA1 --off" 
