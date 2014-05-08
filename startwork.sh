8e4f0e77a22be9c2df79d14094c8838bf42e149c
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-05-08 09:04:50 +0200

    Adding mopidy as a systemd service, update start/stop work

--- startwork.sh
+++ startwork.sh
@@ -5,3 +5,4 @@ kstart skype &
 kstart pidgin & 
 truecrypt /dev/sda4 -p=bs8b@_@kmM & 
 kstart firefox https://outlook.com/here.com &
+systemctl --user start mopidy
--- stopwork.sh
+++ stopwork.sh
@@ -4,5 +4,7 @@ kill `ps ax | grep qtcre | grep Shoo | awk {'print $1'}`
 killall skype
 killall pidgin
 killall firefox
+killall kmail
 nmcli con down id mob-wawa
 nmcli con down id Nokia
+systemctl --user stop mopidy
