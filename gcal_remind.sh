db9b644f04bd1fbefa7bc19780f9a4870c7773af
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-06-10 10:09:18 +0200

    Adding gcal remind script as cron doesn't work

---
+++ gcal_remind.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while [ true ]
+do
+    echo "Remind me"
+    /usr/bin/gcalcli remind 15 'notify-send -i /usr/share/icons/oxygen/48x48/actions/view-calendar-whatsnext.png -u critical -a gcalcli %s'
+    sleep 120
+done
