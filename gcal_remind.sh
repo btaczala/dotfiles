d2b0c7106941979d1ec4f30df42984f2068b3f20
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-06-13 10:26:04 +0200

    GCal remind with lesser timespan

--- gcal_remind.sh
+++ gcal_remind.sh
@@ -4,5 +4,5 @@ while [ true ]
 do
     echo "Remind me"
     /usr/bin/gcalcli remind 15 'notify-send -i /usr/share/icons/oxygen/48x48/actions/view-calendar-whatsnext.png -u critical -a gcalcli %s'
-    sleep 120
+    sleep 600
 done
