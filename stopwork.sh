a776fc44d7c6dd4a9f84316eea970587a6b16033
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-07-08 08:55:16 +0200

    Changes in start/stop work

--- stopwork.sh
+++ stopwork.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+
+function close_program()
+{
+    progName=$1
+    if [[ -n `pidof $progName` ]]; then
+        kill -2 `pidof $progName`
+    fi
+}
+
 kill `ps ax | grep qtcre | grep Shoo | awk {'print $1'}`
-killall skype
-killall pidgin
-killall firefox
-killall kmail
+~/dotfiles/skype_away.py offline
+sleep 2
+purple-remote quit
+
+close_program firefox
+sleep 1
+akonadictl stop
+close_program kmail
 nmcli con down id mob-wawa
 nmcli con down id Nokia
 systemctl --user stop mopidy
+killall skype
