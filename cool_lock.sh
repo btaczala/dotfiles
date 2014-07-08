93a797c82c99f1219b22dbd31dcc689c4b7e3576
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-07-08 08:55:02 +0200

    Changing pidgin status on lock/unlock

--- cool_lock.sh
+++ cool_lock.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ~/dotfiles/skype_away.py away
+purple-remote "setstatus?status=away&message=AFK"
 i3lock -f
 ~/dotfiles/skype_away.py online
+purple-remote "setstatus?status=available&message="
