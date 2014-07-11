2f64d4e6ccc398ada159e407d9d43af968a09c66
Bartosz Taczala <bartosz.taczala@mobica.com>
2014-07-11 11:27:00 +0200

    Switch frm kmail to mutt/offlineimap

--- startwork.sh
+++ startwork.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-kstart kmail & 
-akonadictl start
 kstart skype & 
 kstart pidgin & 
 truecrypt /dev/sda4 -p=bs8b@_@kmM & 
 kstart firefox https://outlook.com/here.com &
 systemctl --user start mopidy
+systemctl --user start offlineimap.service
+systemctl --user start offlineimap.timer
