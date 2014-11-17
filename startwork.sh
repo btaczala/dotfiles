4e47ed94381ca94cacac0c72c317375be843163e
Bartosz Taczala <ext-bartosz.1.taczala@here.com>
2014-11-17 08:51:35 +0100

    Small improvs

--- startwork.sh
+++ startwork.sh
@@ -4,10 +4,9 @@ function start() {
     nohup $1 > /dev/null 2>&1 &
 }
 
+start pidgin
 start skype
-start pidgin 
-truecrypt /dev/sda4 -p=bs8b@_@kmM & 
-systemctl --user start mopidy
+
 systemctl --user start gcalcli.timer
 sleep 5
 ~/dotfiles/skype_away.py online
--- sup/hooks/before-poll.rb
+++ sup/hooks/before-poll.rb
@@ -1,2 +1,4 @@
-say "Running offlineimap..."
+say "Running offlineimap gmail..."
 system "offlineimap", "-o", "-u", "quiet", "-c", "~/dotfiles/sup/offlineimap"
+say "Running offlineimap here..."
+system "offlineimap", "-o", "-u", "quiet", "-c", "~/dotfiles/sup/offlineimap_imap"
