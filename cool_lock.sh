8810a9776314818c88635cfccf1979e204d5e7e1
Bartosz Taczala <ext-bartosz.1.taczala@here.com>
2014-12-03 21:44:04 +0100

    Update both accounts here and gmail

--- cool_lock.sh
+++ cool_lock.sh
@@ -3,6 +3,6 @@
 purple-remote "setstatus?status=away&message=AFK"
 scrot /tmp/screenshot.png
 convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+i3lock -n -i /tmp/screenshotblur.png
 ~/dotfiles/skype_away.py online
 purple-remote "setstatus?status=available&message="
--- sup/hooks/before-poll.rb
+++ sup/hooks/before-poll.rb
@@ -1,2 +1,4 @@
-say "Running offlineimap..."
+say "Running offlineimap for gmail"
 system "offlineimap", "-o", "-u", "quiet", "-c", "~/dotfiles/sup/offlineimap"
+say "Running offlineimap for here"
+system "offlineimap", "-o", "-u", "quiet", "-c", "~/dotfiles/sup/offlineimap_here"
