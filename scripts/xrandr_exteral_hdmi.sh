5e496a63b8a930100d08419217fce12ea02bc265
w0land <bartek.taczala@gmail.com>
2014-12-10 00:01:48 +0100

    Adding scripts to change xrandr and adapt bspwm

--- scripts/xrandr_exteral_hdmi.sh
+++ scripts/xrandr_exteral_hdmi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # kill trayer
-killall trayer
+killall stalonetray
 # set monitor
 xrandr --output eDP1 --off --output HDMI1 --auto
 # add padding
 bspc config top_padding 15
-trayer --edge top --width 100 --widthtype pixel --align left --height 13 --margin 60 --tint 0xFF1f1f1f --transparent true --alpha 0 &
+stalonetray --dockapp-mode simple &
