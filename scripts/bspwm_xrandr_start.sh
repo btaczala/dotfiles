248b69d0c8dbb2c547d5017c796fd8a6fa39304c
w0land <bartek.taczala@gmail.com>
2014-12-17 23:05:35 +0100

    Lots of changes

--- scripts/bspwm_xrandr_start.sh
+++ scripts/bspwm_xrandr_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-~/.config/bspwm/panel/panel
+~/.config/bspwm/panel/panel &
 sleep 2
 stalonetray --dockapp-mode simple &
-feh --bg-scale ~/ownCloud/wallpapers/1616699.jpg
+feh --bg-scale $WALLPAPER &
 conky -c ~/dotfiles/conky/gcalcli &
 conky -c ~/dotfiles/conky/panel &
--- scripts/xrandr_edp.sh
+++ scripts/xrandr_edp.sh
@@ -6,4 +6,4 @@ xrandr --output eDP1 --auto --output HDMI1 --off
 # add padding
 bspc config top_padding 15
 bspc monitor -d term web mail dev im vi vii viii ix x
-~/dotfiles/scripts/bspwm_xrandr_start.sh
+~/dotfiles/scripts/bspwm_xrandr_start.sh & 
