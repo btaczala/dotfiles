8fc3707d8a87ac56c739d2536facad49751ba28e
Bartek Taczała <bartek.taczala@gmail.com>
2015-01-13 12:11:57 +0100

    Small changes

--- scripts/xrandr_hdmi_vga.sh
+++ scripts/xrandr_hdmi_vga.sh
@@ -6,8 +6,8 @@ xrandr --output LVDS1 --off --output VGA1 --auto
 xrandr --output HDMI1 --auto --output VGA1 --auto --output VGA1 --right-of HDMI1
 
 bspc monitor 1 -n VGA1
-bspc monitor VGA1 -d 1/VGA1/mail 2/VGA1/web 3/VGA1/im 4/VGA1/term
-bspc monitor HDMI1 -d 5/HDMI1/dev 6/HDMI1/git 7/HDMI1/term
+bspc monitor VGA1 -d 1/VGA1/mail 2/VGA1/web 3/VGA1/im 4/VGA1/term 5/VGA1/VM
+bspc monitor HDMI1 -d 6/HDMI1/dev 7/HDMI1/git 8/HDMI1/term
 
 # custom rules bitches
 bspc rule -r Pidgin
