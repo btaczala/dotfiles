4e104519b0b83e6decac647a770e4920d0356822
Bartek Taczała <bartek.taczala@gmail.com>
2015-01-14 23:51:51 +0100

    After staring work sync drive

--- startwork.sh
+++ startwork.sh
@@ -4,6 +4,11 @@ function start() {
     nohup $@ > /dev/null 2>&1 &
 }
 
+echo 'Syncing drive'
+cd ~/drive
+drive pull -r --no-prompt=true
+cd -
+
 start pidgin
 start skype
 systemctl --user start mopidy
