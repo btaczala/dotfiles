b745a8f217ff98c62d47468bdf0c07c6daead984
Bartek Taczała <bartek.taczala@gmail.com>
2015-01-27 07:27:50 +0100

    i3lock: don't blur

--- cool_lock.sh
+++ cool_lock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ~/dotfiles/skype_away.py away
 purple-remote "setstatus?status=away&message=AFK"
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -n -i /tmp/screenshotblur.png
+#scrot /tmp/screenshot.png
+#convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+#i3lock -n -i /tmp/screenshotblur.png
+i3lock -t -n -i ~/drive/wallpapers/orK22AE.png
 ~/dotfiles/skype_away.py online
 purple-remote "setstatus?status=available&message="
