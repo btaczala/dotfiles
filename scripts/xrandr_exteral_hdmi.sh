f3c2aefa45b22c5bd98b289ae9b76d9c4e078b56
Bartek Taczała <bartek.taczala@gmail.com>
2015-01-28 23:56:51 +0100

    Changes in script

--- scripts/xrandr_exteral_hdmi.sh
+++ scripts/xrandr_exteral_hdmi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# kill trayer
-killall stalonetray
+~/dotfiles/scripts/bspwm_xrandr_stop.sh
 # set monitor
 xrandr --output eDP1 --off --output HDMI1 --auto
 # add padding
 bspc config top_padding 15
-stalonetray --dockapp-mode simple &
+bspc monitor -d term web mail dev im vi vii viii ix x
+~/dotfiles/scripts/bspwm_xrandr_start.sh & 
