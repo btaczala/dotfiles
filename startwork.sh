964aa34f88620d4164c06846f915e8c17e154f21
Bartek Taczała <bartek.taczala@gmail.com>
2015-02-25 11:15:30 +0100

    Move sync to end of start work

--- startwork.sh
+++ startwork.sh
@@ -4,11 +4,6 @@ function start() {
     nohup $@ > /dev/null 2>&1 &
 }
 
-echo 'Syncing drive'
-cd ~/drive
-drive pull -r --no-prompt=true
-task sync
-cd -
 
 termite -t mutt -d ~/Downloads -e "mutt -F ~/dotfiles/mutt/mutt_mobica" & 
 start pidgin
@@ -17,3 +12,9 @@ systemctl --user start mopidy
 systemctl --user start gcalcli.timer
 sleep 5
 ~/dotfiles/skype_away.py online
+
+echo 'Syncing drive'
+cd ~/drive
+drive pull -r --no-prompt=true
+task sync
+cd -
