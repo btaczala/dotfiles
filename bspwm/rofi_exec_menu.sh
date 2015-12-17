aa371a5e27821f7269afd68b6029b079db695d41
Bartosz Taczała <bartosz.taczala@mobica.com>
2015-12-17 12:42:25 +0100

    Some changes

--- bspwm/rofi_exec_menu.sh
+++ bspwm/rofi_exec_menu.sh
@@ -10,5 +10,5 @@ curr_width=$( printf "%.0f" $t )
 x=`echo "($width-$curr_width)/2" | bc`
 y=`echo "$height/2 - 50" | bc`
 
-dmenu_run -x $x -y $y -w $curr_width -l 10 -nb "#282828" -nf "#ccb18b" -fn Terminess-8:normal
+PATH=$PATH:$HOME/bin dmenu_run -x $x -y $y -w $curr_width -l 10 -nb "#282828" -nf "#ccb18b" -fn Terminess-8:normal
 #echo $curr_width " " $x
