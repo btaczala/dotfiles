bded83d4bad76474ae80ede89acf15e5ac5f6cb6
Bartosz Taczała <bartosz.taczala@mobica.com>
2016-12-29 07:05:58 +0100

    Tmux: Simplify right bar
    I really don't need next meeting nor battery stats. Really.
    But the script may come in handy :)

--- tmux/next_meeting.sh
+++ tmux/next_meeting.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gcalcli agenda --nocolor | cut -d " " -f 4- | head -2 | tail -1 | sed "s/^ *//g" | sed "s/    / /g" | cut -c1-19
+today=`date +"%m/%d/%y"`
+tomorrow=`date --date=tomorrow +"%m/%d/%y"`
+gcalcli agenda --nostarted --nocolor $today $tomorrow | cut -d " " -f 4- | head -2 | tail -1 | sed "s/^ *//g" | sed "s/    / /g" | cut -c1-19
