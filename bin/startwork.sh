ff6de04c4cec41fe4a9f713c5d50fd955c7952cc
Bartosz Taczała <bartosz.taczala@mobica.com>
2016-12-29 07:04:46 +0100

    Work: Don't start mail termite

--- bin/startwork.sh
+++ bin/startwork.sh
@@ -11,13 +11,6 @@ systemctl --user start mopidy
 systemctl --user start gcalcli.timer
 systemctl --user start getmail.timer 
 
-out=`ps ax | grep "termite -e tmuxinator start mails" | grep -v grep`
-if [ "$out" == "" ]; then
-    termite  -e 'tmuxinator start mails' --name=mutt  --class=mutt &
-else
-    echo "Mutt is running"
-fi
-
 out=`ps ax | grep /opt/google/chrome/chrome | grep -v grep`
 if [ "$out" == "" ]; then
     google-chrome-stable --profile-directory="Profile 4" --class=chrome_priv &
