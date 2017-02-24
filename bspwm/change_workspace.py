143ded9d11e3b1257b73fd469c28b7ec026ce2f0
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-02-24 09:00:54 +0100

    Multi monitor support in change_workspace

--- bspwm/change_workspace.py
+++ bspwm/change_workspace.py
@@ -2,18 +2,18 @@ from subprocess import Popen, PIPE
 
 process = Popen(['/usr/bin/bspc', 'query', '-M'], stdout=PIPE)
 stdout = process.communicate()[0]
-monitor = stdout.decode("utf-8")[:-1]
+monitors = stdout.decode("utf-8")[:-1]
+print(monitors)
 
+workspaces = ""
+for monitor in monitors.splitlines(): 
+    process = Popen(['/usr/bin/bspc', 'query', '-D', '-m', monitor], stdout=PIPE)
+    stdout = process.communicate()[0]
+    workspaces += stdout.decode("utf-8")[:-1]
+    workspaces += "\n"
 
-process = Popen(['/usr/bin/bspc', 'query', '-D', '-m', monitor], stdout=PIPE)
-stdout = process.communicate()[0]
-monitor = stdout.decode("utf-8")[:-1]
-
-# for desktop in monitor.splitlines():
-    # print (desktop)
-
-p = Popen(['/usr/bin/rofi', '-dmenu', '-i'], stdin=PIPE, stdout=PIPE)
-stdout = p.communicate(input=monitor.encode())[0]
+p = Popen(['/usr/bin/rofi', '-dmenu', '-i', '-p', 'select workspace:'], stdin=PIPE, stdout=PIPE)
+stdout = p.communicate(input=workspaces.encode())[0]
 required = stdout.decode("utf-8")[:-1]
 
 print (required)
