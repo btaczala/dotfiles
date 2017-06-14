2c4aec73537e85ff841402d4009098fe87fccea8
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-14 09:58:02 +0200

    Adding irc and spotify spaces

--- monitors/laptop/bspwm.sh
+++ monitors/laptop/bspwm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bspc monitor -d mail web im dev code system ssh priv 
+bspc monitor -d mail web im irc music dev code system priv  
 bspc config top_padding 20
--- monitors/work/bspwm.sh
+++ monitors/work/bspwm.sh
@@ -2,8 +2,8 @@
 
 source $HOME/dotfiles/monitors/`hostname`.config
 
-bspc monitor $work_monitor_right -d mail web im system dev docs
-bspc monitor $work_monitor_left -d code dev ssh priv
+bspc monitor $work_monitor_right -d mail web im system irc vm
+bspc monitor $work_monitor_left -d code dev music priv
 
 # custom rules bitches
 bspc rule -r Pidgin
