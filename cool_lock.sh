bf3c73bb0a860ecc6f8a7a39c5799cbe6e4498a4
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-06-28 09:02:39 +0200

    lock: Adding irc away status

--- cool_lock.sh
+++ cool_lock.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+function irc_away
+{
+    msg=$1
+    fifo=`find $HOME/.weechat -name weechat_fifo*` 
+    if [[ ! "$fifo" == "" ]]; then
+        echo "irc.server.jlr */away $msg" > $fifo
+    fi
+}
+
 icon="$HOME/dotfiles/lock.png"
 tmpbg='/tmp/screenshot.png'
 
 mpc_status=`mpc status | grep playing`
 
+irc_away "away"
+
 if [ ! "$mpc_status" == "" ]; then
     mpc pause
 fi
 
-echo "xwobf"
 /usr/bin/xwobf -s 5 $tmpbg
 i3lock -n -i $tmpbg
-purple-remote "setstatus?status=available&message="
+irc_away ""
 rm $tmpbg
 
 if [ ! "$mpc_status" == "" ]; then
