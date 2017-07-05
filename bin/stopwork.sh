15c7c895a6026d7a6c0aa5ba583c88a52226ba78
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-07-05 10:25:22 +0200

    Simplify stopwork

--- bin/stopwork.sh
+++ bin/stopwork.sh
@@ -8,19 +8,9 @@ function close_program()
         kill -2 `pidof $progName`
     fi
 }
-
-purple-remote "setstatus?status=offline&message="
-~/dotfiles/skype_away.py offline
-nmcli con down id here.com
-sleep 2
 systemctl --user stop mopidy
-systemctl --user stop getmail.timer
-systemctl --user stop getmail.service
-close_program skype
-pkill --oldest chrome
-pkill --oldest skype
 close_program ncmpcpp
-purple-remote quit
+killall skypeforlinux
 
 # kill all terminals
 killall termite
