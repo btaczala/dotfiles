4a8fbdced5a211f71b08156e297ceec6192e0517
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-01-04 13:21:10 +0100

    zsh: extra fzf functions

--- zsh/my_fzf.sh
+++ zsh/my_fzf.sh
@@ -29,3 +29,18 @@ fshow() {
                 {}
 FZF-EOF"
 }
+
+z() {
+  local dir
+  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
+
+# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fbr() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
