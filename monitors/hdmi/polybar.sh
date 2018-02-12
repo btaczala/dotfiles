0bc304abda31a525410be9d215f592ed4c1225a2
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-02-12 16:48:17 +0100

    [host configs] Add battery, adapter, wireless, eth

--- monitors/hdmi/polybar.sh
+++ monitors/hdmi/polybar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+source $HOME/dotfiles/hostconfig/$(hostname)
 #polybar only_one -c ~/dotfiles/lemonbuddy/thinkpad/poly_config -r & 
 BAR_MONITOR=HDMI-3 polybar main -c ~/dotfiles/lemonbuddy/thinkpad/dual -r &
