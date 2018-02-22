a289522e89fcadab04ea9c8b5bbeab90982591aa
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-02-22 07:32:42 +0100

    [sxhkd] Use correct autorandr invocation

--- bin/startwork.sh
+++ bin/startwork.sh
@@ -14,7 +14,6 @@ systemctl --user start getmail.timer
 out=`ps ax | grep /opt/google/chrome/chrome | grep -v grep`
 if [ "$out" == "" ]; then
     google-chrome-stable --profile-directory="Profile 4" --class=chrome_priv &
-    google-chrome-stable --profile-directory="Profile 1" --class=chrome_work &
 else
     echo "Chrome is running"
 fi
