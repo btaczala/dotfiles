b3e8d08510af91547247d1dbbeccd2eaf676577c
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-03-02 12:57:46 +0100

    polybar: Add indicator if device is connected via kdedconnect

---
+++ lemonbuddy/thinkpad/nexus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! pgrep -x "kdeconnectd" > /dev/null 
+then
+    exit 1
+fi
+
+id=$(dbus-send --session --dest=org.kde.kdeconnect --print-reply /modules/kdeconnect org.kde.kdeconnect.daemon.deviceIdByName string:"Bartek-Nexus" | tail -n 1 | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+
+if [[ $id == "" ]]; then
+    exit 1
+fi
+
+is_reachable=$(dbus-send --session --dest=org.kde.kdeconnect --print-reply /modules/kdeconnect/devices/$id org.freedesktop.DBus.Properties.Get string:org.kde.kdeconnect.device string:isReachable | tail -1 | awk '{print $3}')
+
+if [[ "$is_reachable" == "true" ]]; then
+    exit 0
+fi
+
+exit 1
+
