c203fe2c408ed9fa75f5cf4471fb014747cfdac9
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-11-28 21:21:19 +0100

    zsh: remove fd alias
    fd is super tool!

--- zsh/my_fzf.sh
+++ zsh/my_fzf.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
-
 # fkill - kill process
 fkill() {
   local pid
