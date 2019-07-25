16e22ff2ffdaccadca4b4bb0f6e5e921fe8b0df2
Bartosz Taczała <bartosz.taczala@siili.com>
2019-07-25 17:56:18 +0200

    qutebrowser: Init config

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,8 @@
+
+c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}", "wa": "https://wiki.archlinux.org/?search={}"}
+c.auto_save.session = True
+config.bind('d', 'scroll-page 0 0.5 ')
+config.bind('x', 'tab-close')
+config.bind('<ctrl-b>', 'buffer')
+
+c.fonts.tabs="Unifont, Medium 8"
