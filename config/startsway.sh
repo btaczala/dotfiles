c5ef36459dc2fa3da9a8166bfeaf2b13331bba99
Bartosz Taczała <bartosz.taczala@siili.com>
2019-08-07 17:43:27 +0200

    sway: Use wayland for Qt apps (ALWAYS)

--- config/startsway.sh
+++ config/startsway.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export QT_QPA_PLATFORMTHEME=qt5ct
-# this fixes an issue where Qt apps scale like shit 
+# this fixes an issue where Qt apps scale like shit
 # after suspend/resume combo
+export QT_QPA_PLATFORM=wayland
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export _JAVA_AWT_WM_NONREPARENTING=1
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
