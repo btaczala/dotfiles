8dfb7f5e0044e73c9b5b1f3ad2fb4cbcecd6ea47
Bartosz Taczała <bartosz.taczala@siili.com>
2019-09-12 10:55:34 +0200

    qutebrowser: Spawn qute-pass

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,8 +1,9 @@
 
-c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}", "wa": "https://wiki.archlinux.org/?search={}"}
+c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}", "wa": "https://wiki.archlinux.org/?search={}", "g": "https://google.com/search?q={}"}
 c.auto_save.session = True
 config.bind('d', 'scroll-page 0 0.5 ')
 config.bind('x', 'tab-close')
 config.bind('<ctrl-b>', 'buffer')
+config.bind(',p', 'spawn --userscript qute-pass --dmenu-invocation dmenu')
 
 c.fonts.tabs="Unifont, Medium 8"
