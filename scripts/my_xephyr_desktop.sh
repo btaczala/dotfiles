6a6d417054ab8a166fbaffe49ce41b3aea80d653
Bartosz Taczała <bartosz.taczala@siili.com>
2019-09-27 09:52:56 +0200

    xephyr: Fix screen size

--- scripts/my_xephyr_desktop.sh
+++ scripts/my_xephyr_desktop.sh
@@ -6,10 +6,10 @@ sleep 1
 s_width=$(swaymsg -t get_outputs -r | jq '.[0].rect.width')
 s_height=$(swaymsg -t get_outputs -r | jq '.[0].rect.height')
 
-width=$(echo "$s_width-200" | bc)
-height=$(echo "$s_height-200" | bc)
+width=$(echo "$s_width-40" | bc)
+height=$(echo "$s_height-40" | bc)
 
 echo "Xephyr :1 -screen "$width"x"$height" -dpi 96 -ac &"
 Xephyr :1 -screen "$width"x"$height" -dpi 96 -ac &
 sleep 1
-DISPLAY=:1 openbox & 
+DISPLAY=:1 openbox &
