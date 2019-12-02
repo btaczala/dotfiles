43b895a70fba79751e2f866fcea27be542cbc4dd
Bartosz Taczała <bartosz.taczala@siili.com>
2019-12-02 13:18:21 +0100

    lock: Lock pgp pass when screen is locked unlock when screen is unlocked

--- cool_lock_sway.sh
+++ cool_lock_sway.sh
@@ -14,6 +14,14 @@ function revert() {
     fi
 }
 
+function forget_gpg_pass() {
+    echo RELOADAGENT | gpg-connect-agent
+}
+
+function unlock_gpg_pass() {
+    swaynag -m "remember about pgp" -t warning -e bottom -B unlock "pass private/adguard > /dev/null && killall swaynag"
+}
+
 trap revert HUP INT TERM
 
 if [ "$mpc_status" == "[playing]" ]; then
@@ -25,7 +33,10 @@ background=~/dotfiles/walls/trees.jpg
 tmpbg=$(mktemp)
 convert -composite $background $HOME/dotfiles/rick.png -gravity South -geometry -20x1200 $background $tmpbg
 convert -pointsize 48 -font /usr/share/fonts/TTF/Hack-Regular.ttf -fill white -draw 'text 200,320 "I am sorry but Your opinion means very little to me"' $tmpbg $tmpbg
+
+forget_gpg_pass
 swaylock $tmpbg --image $tmpbg $@
+unlock_gpg_pass
 
 rm $tmpbg
 revert
