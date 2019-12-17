f3c381ef3c5f3aaf75c89153191520f76b62e9a8
Bartosz Taczała <bartosz.taczala@siili.com>
2019-12-17 12:34:53 +0100

    lock: Pause mpv as well

--- cool_lock_sway.sh
+++ cool_lock_sway.sh
@@ -6,37 +6,30 @@ if pgrep -x "swaylock" >/dev/null; then
     exit 1
 fi
 
-mpc_status="$(mpc status | grep playing | awk '{print $1}')"
+#mpc_status="$(mpc status | grep playing | awk '{print $1}')"
+mpc_status="$(playerctl status 2>/dev/null)"
+
+was_playing() {
+    if [ "$mpc_status" = "[playing]" ] | [ "$mpc_status" = "Playing" ]; then
+        return 0
+    fi
+    return 1
+}
 
 function revert() {
-    if [ "$mpc_status" == "[playing]" ]; then
-        mpc play
-    fi
-}
-
-function forget_gpg_pass() {
-    echo RELOADAGENT | gpg-connect-agent
-}
-
-function unlock_gpg_pass() {
-    swaynag -m "remember about pgp" -t warning -e bottom -B unlock "pass private/adguard > /dev/null && killall swaynag"
+    if was_playing; then playerctl play; fi
 }
 
 trap revert HUP INT TERM
 
-if [ "$mpc_status" == "[playing]" ]; then
-    mpc pause
-fi
+if was_playing; then playerctl pause; fi
 
-#grim $tmpbg
 background=~/dotfiles/walls/trees.jpg
 tmpbg=$(mktemp)
 convert -composite $background $HOME/dotfiles/rick.png -gravity South -geometry -20x1200 $background $tmpbg
 convert -pointsize 48 -font /usr/share/fonts/TTF/Hack-Regular.ttf -fill white -draw 'text 200,320 "I am sorry but Your opinion means very little to me"' $tmpbg $tmpbg
 
-forget_gpg_pass
 swaylock $tmpbg --image $tmpbg $@
-unlock_gpg_pass
 
 rm $tmpbg
 revert
