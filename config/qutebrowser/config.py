b06711ec3301b029ac0511e00430040af2b0a6a4
Bartosz Taczała <bartosz.taczala@siili.com>
2019-12-31 13:03:24 +0100

    qutebrowser: pass

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -4,6 +4,10 @@ c.auto_save.session = True
 config.bind('d', 'scroll-page 0 0.5 ')
 config.bind('x', 'tab-close')
 config.bind('<ctrl-b>', 'buffer')
-config.bind(',p', 'spawn --userscript qute-pass --dmenu-invocation dmenu')
+
+config.bind('<z><l>', 'spawn --userscript qute-pass')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
+config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
+config.bind('<z><o><l>', 'spawn --userscript qute-pass --otp-only')
 
 c.fonts.tabs="Unifont, Medium 8"
