a423f54fd6c550d496039b579e1d7c59c9e6f803
Bartosz Taczała <bartosz.taczala@siili.com>
2020-04-15 11:12:21 +0200

    qutebrowser: MPV on youtube

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -6,6 +6,9 @@ config.bind('x', 'tab-close')
 config.bind('<ctrl-b>', 'buffer')
 
 config.bind('<z><l>', 'spawn --userscript qute-pass')
+config.bind(',m', 'spawn mpv --title=mpv_youtube {url}')
+config.bind(',M', 'hint links spawn mpv --title=mpv_youtube mpv {hint-url}')
+
 config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
 config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
 config.bind('<z><o><l>', 'spawn --userscript qute-pass --otp-only')
