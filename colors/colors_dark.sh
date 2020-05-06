b997d019c555ce64dc3d50f52fdbea4b04a83998
Bartosz Taczała <bartosz.taczala@siili.com>
2020-05-06 18:52:05 +0200

    sway: Configs

--- colors/colors_dark.sh
+++ colors/colors_dark.sh
@@ -13,3 +13,7 @@ colors_gtk "Materia-dark" "Flat-Remix-Green-Dark"
 colors_rofi "dark"
 colors_qt darker.conf Flat-Remix-Green-Dark
 color_slack "dark"
+
+pkill swaybg
+swaybg -o DP-2 -i '/home/bartek/dotfiles/walls/LazyDaysII.jpg' -m fill &
+swaybg -o HDMI-A-1 -i '/home/bartek/dotfiles/walls/planets.jpg' -m fill &
--- colors/colors_light.sh
+++ colors/colors_light.sh
@@ -13,3 +13,7 @@ colors_gtk "Materia-light" "Flat-Remix-Green-Light"
 colors_rofi "light"
 colors_qt airy.conf Flat-Remix-Green-Light
 color_slack "light"
+
+pkill swaybg
+swaybg -o DP-2 -i '/home/bartek/dotfiles/walls/LazyDaysII.jpg' -m fill &
+swaybg -o HDMI-A-1 -i '/home/bartek/dotfiles/walls/Beautiful-Lake-4K-Wallpaper-3840x2160.jpg' -m fill &
