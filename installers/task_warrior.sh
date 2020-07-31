189ed340b6350cedf10d3fe51845daff4350108f
Bartosz Taczała <bartosz.taczala@siili.com>
2020-07-31 18:40:58 +0200

    installers: Proper task_warrior install

--- installers/task_warrior.sh
+++ installers/task_warrior.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-ovh_server=vps513306.ovh.net
+ovh_server=vps762509.ovh.net
+task_user="bartek"
 
 function force_setup_task_warrior() {
     task_dir=$1
     echo "Creating $task_dir"
     mkdir -p $task_dir
-    scp bartek@vps513306.ovh.net:~/bartek_taczała.cert.pem $task_dir/
-    scp bartek@vps513306.ovh.net:~/bartek_taczała.key.pem $task_dir/
-    scp bartek@vps513306.ovh.net:~/ca.cert.pem $task_dir/
+    scp bartek@$ovh_server:~/$task_user.cert.pem $task_dir/
+    scp bartek@$ovh_server:~/$task_user.key.pem $task_dir/
+    scp bartek@$ovh_server:~/ca.cert.pem $task_dir/
 
-    yes | task config taskd.certificate   -- $task_dir/bartek_taczała.cert.pem
-    yes | task config taskd.key           -- $task_dir/bartek_taczała.key.pem
+    yes | task config taskd.certificate   -- $task_dir/$task_user.cert.pem
+    yes | task config taskd.key           -- $task_dir/$task_user.key.pem
     yes | task config taskd.ca            -- $task_dir/ca.cert.pem
 
     yes | task config taskd.server        -- $ovh_server:53589
-    yes | task config taskd.credentials   -- private/Bartek Taczała/46ba2358-dc51-48fc-8fc5-22bfc6efcb0a
+    yes | task config taskd.credentials   -- private/bartek/79d7ada1-23c6-4e1e-8409-22a714d17daf
 
     yes | task sync init
     yes | task sync
@@ -24,7 +25,7 @@ function force_setup_task_warrior() {
 function setup_task_warrior() {
     task_dir=$HOME/.task
     taskrc=$HOME/.taskrc
-    if [ -d $task_dir ]; then 
+    if [ -d $task_dir ]; then
         read -p "$task_dir exists, do you want to remove it (y/n)" CONT
         if [ "$CONT" = "y" ]; then
             rm $task_dir -rfv
