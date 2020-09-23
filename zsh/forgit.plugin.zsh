99100c6ce3f96c58fef0495099adbc040ab28fe8
Bartosz Taczała <bartosz.taczala@siili.com>
2020-09-23 23:10:35 +0200

    zsh: bump forgit

--- zsh/forgit.plugin.zsh
+++ zsh/forgit.plugin.zsh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # MIT (c) Wenxuan Zhang
 forgit::warn() { printf "%b[Warn]%b %s\n" '\e[0;33m' '\e[0m' "$@" >&2; }
 forgit::info() { printf "%b[Info]%b %s\n" '\e[0;32m' '\e[0m' "$@" >&2; }
@@ -6,13 +7,17 @@ forgit::inside_work_tree() { git rev-parse --is-inside-work-tree >/dev/null; }
 # https://github.com/wfxr/emoji-cli
 hash emojify &>/dev/null && forgit_emojify='|emojify'
 
-forgit_pager=$(git config core.pager || echo 'cat')
+forgit_pager=${FORGIT_PAGER:-$(git config core.pager || echo 'cat')}
+forgit_show_pager=${FORGIT_SHOW_PAGER:-$(git config pager.show || echo "$forgit_pager")}
+forgit_diff_pager=${FORGIT_DIFF_PAGER:-$(git config pager.diff || echo "$forgit_pager")}
+forgit_ignore_pager=${FORGIT_IGNORE_PAGER:-$(hash bat &>/dev/null && echo 'bat -l gitignore --color=always' || echo 'cat')}
 
 # git commit viewer
 forgit::log() {
     forgit::inside_work_tree || return 1
-    local cmd opts graph
-    cmd="echo {} |grep -Eo '[a-f0-9]+' |head -1 |xargs -I% git show --color=always % $* | $forgit_pager"
+    local cmd opts graph files
+    files=$(sed -nE 's/.* -- (.*)/\1/p' <<< "$*") # extract files parameters for `git show` command
+    cmd="echo {} |grep -Eo '[a-f0-9]+' |head -1 |xargs -I% git show --color=always % -- $files | $forgit_show_pager"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         +s +m --tiebreak=index
@@ -37,10 +42,8 @@ forgit::diff() {
             files=("$@")
         fi
     }
-
     repo="$(git rev-parse --show-toplevel)"
-    target="\$(echo {} | sed 's/.*]  //')"
-    cmd="git diff --color=always $commit -- $repo/$target | $forgit_pager"
+    cmd="echo {} |sed 's/.*]  //' |xargs -I% git diff --color=always $commit -- '$repo/%' | $forgit_diff_pager"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         +m -0 --bind=\"enter:execute($cmd |LESS='-R' less)\"
@@ -54,13 +57,12 @@ forgit::diff() {
 forgit::add() {
     forgit::inside_work_tree || return 1
     # Add files if passed as arguments
-    [[ $# -ne 0 ]] && git add "$@" && return
+    [[ $# -ne 0 ]] && git add "$@" && git status -su && return
 
     local changed unmerged untracked files opts preview extract
     changed=$(git config --get-color color.status.changed red)
     unmerged=$(git config --get-color color.status.unmerged red)
     untracked=$(git config --get-color color.status.untracked red)
-
     # NOTE: paths listed by 'git status -su' mixed with quoted and unquoted style
     # remove indicators | remove original path for rename case | remove surrounding quotes
     extract="
@@ -70,9 +72,9 @@ forgit::add() {
     preview="
         file=\$(echo {} | $extract)
         if (git status -s -- \$file | grep '^??') &>/dev/null; then  # diff with /dev/null for untracked files
-            git diff --color=always --no-index -- /dev/null \$file | $forgit_pager | sed '2 s/added:/untracked:/'
+            git diff --color=always --no-index -- /dev/null \$file | $forgit_diff_pager | sed '2 s/added:/untracked:/'
         else
-            git diff --color=always -- \$file | $forgit_pager
+            git diff --color=always -- \$file | $forgit_diff_pager
         fi"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
@@ -92,7 +94,7 @@ forgit::add() {
 forgit::reset::head() {
     forgit::inside_work_tree || return 1
     local cmd files opts
-    cmd="git diff --cached --color=always -- {} | $forgit_pager "
+    cmd="git diff --cached --color=always -- {} | $forgit_diff_pager "
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         -m -0
@@ -107,7 +109,7 @@ forgit::reset::head() {
 forgit::restore() {
     forgit::inside_work_tree || return 1
     local cmd files opts
-    cmd="git diff --color=always -- {} | $forgit_pager"
+    cmd="git diff --color=always -- {} | $forgit_diff_pager"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         -m -0
@@ -122,7 +124,7 @@ forgit::restore() {
 forgit::stash::show() {
     forgit::inside_work_tree || return 1
     local cmd opts
-    cmd="git stash show \$(echo {}| cut -d: -f1) --color=always --ext-diff | $forgit_pager"
+    cmd="echo {} |cut -d: -f1 |xargs -I% git stash show --color=always --ext-diff % |$forgit_diff_pager"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         +s +m -0 --tiebreak=index --bind=\"enter:execute($cmd | LESS='-R' less)\"
@@ -141,22 +143,35 @@ forgit::clean() {
         $FORGIT_CLEAN_FZF_OPTS
     "
     # Note: Postfix '/' in directory path should be removed. Otherwise the directory itself will not be removed.
-    files=$(git clean -xdfn "$@"| sed 's/^Would remove //' | FZF_DEFAULT_OPTS="$opts" fzf |sed 's#/$##')
-    [[ -n "$files" ]] && echo "$files" | tr '\n' '\0' | xargs -0 -I% git clean -xdf '%' && return
+    files=$(git clean -xdffn "$@"| sed 's/^Would remove //' | FZF_DEFAULT_OPTS="$opts" fzf |sed 's#/$##')
+    [[ -n "$files" ]] && echo "$files" | tr '\n' '\0' | xargs -0 -I% git clean -xdff '%' && git status --short && return
     echo 'Nothing to clean.'
+}
+
+forgit::cherry::pick() {
+    local base target preview opts
+    base=$(git branch --show-current)
+    [[ -z $1 ]] && echo "Please specify target branch" && return 1
+    target="$1"
+    preview="echo {1} | xargs -I% git show --color=always % | $forgit_show_pager"
+    opts="
+        $FORGIT_FZF_DEFAULT_OPTS
+        -m -0
+    "
+    git cherry "$base" "$target" --abbrev -v | cut -d ' ' -f2- |
+        FZF_DEFAULT_OPTS="$opts" fzf --preview="$preview" | cut -d' ' -f1 |
+        xargs -I% git cherry-pick %
 }
 
 # git ignore generator
 export FORGIT_GI_REPO_REMOTE=${FORGIT_GI_REPO_REMOTE:-https://github.com/dvcs/gitignore}
-export FORGIT_GI_REPO_LOCAL=${FORGIT_GI_REPO_LOCAL:-~/.forgit/gi/repos/dvcs/gitignore}
+export FORGIT_GI_REPO_LOCAL="${FORGIT_GI_REPO_LOCAL:-${XDG_CACHE_HOME:-$HOME/.cache}/forgit/gi/repos/dvcs/gitignore}"
 export FORGIT_GI_TEMPLATES=${FORGIT_GI_TEMPLATES:-$FORGIT_GI_REPO_LOCAL/templates}
 
 forgit::ignore() {
     [ -d "$FORGIT_GI_REPO_LOCAL" ] || forgit::ignore::update
-    local IFS cmd args cat opts
-    # https://github.com/sharkdp/bat.git
-    hash bat &>/dev/null && cat='bat -l gitignore --color=always' || cat="cat"
-    cmd="$cat $FORGIT_GI_TEMPLATES/{2}{,.gitignore} 2>/dev/null"
+    local IFS cmd args opts
+    cmd="$forgit_ignore_pager $FORGIT_GI_TEMPLATES/{2}{,.gitignore} 2>/dev/null"
     opts="
         $FORGIT_FZF_DEFAULT_OPTS
         -m --preview-window='right:70%'
@@ -164,14 +179,10 @@ forgit::ignore() {
     "
     # shellcheck disable=SC2206,2207
     IFS=$'\n' args=($@) && [[ $# -eq 0 ]] && args=($(forgit::ignore::list | nl -nrn -w4 -s'  ' |
-        FZF_DEFAULT_OPTS="$opts" fzf --preview="$cmd" |awk '{print $2}'))
+        FZF_DEFAULT_OPTS="$opts" fzf --preview="eval $cmd" | awk '{print $2}'))
     [ ${#args[@]} -eq 0 ] && return 1
     # shellcheck disable=SC2068
-    if hash bat &>/dev/null; then
-        forgit::ignore::get ${args[@]} | bat -l gitignore
-    else
-        forgit::ignore::get ${args[@]}
-    fi
+    forgit::ignore::get ${args[@]}
 }
 forgit::ignore::update() {
     if [[ -d "$FORGIT_GI_REPO_LOCAL" ]]; then
@@ -211,6 +222,7 @@ $FZF_DEFAULT_OPTS
 --bind='?:toggle-preview'
 --bind='alt-w:toggle-preview-wrap'
 --preview-window='right:60%'
++1
 $FORGIT_FZF_DEFAULT_OPTS
 "
 
@@ -225,4 +237,5 @@ if [[ -z "$FORGIT_NO_ALIASES" ]]; then
     alias "${forgit_restore:-gcf}"='forgit::restore'
     alias "${forgit_clean:-gclean}"='forgit::clean'
     alias "${forgit_stash_show:-gss}"='forgit::stash::show'
+    alias "${forgit_cherry_pick:-gcp}"='forgit::cherry::pick'
 fi
