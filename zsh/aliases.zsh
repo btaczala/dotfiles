419ed509c30f8a3319e207c7c1fb01ad660a52e7
Bartosz Taczała <bartosz.taczala@siili.com>
2020-11-23 22:28:23 +0100

    zsh: Move away from slow prezto to zplug

---
+++ zsh/aliases.zsh
@@ -0,0 +1,16 @@
+
+alias gst='git status'
+alias gup='git pull --rebase'
+alias ls='exa'
+alias llsa='exa --all -l --grid --header --git'
+alias lls='exa -l --grid --header --git'
+#
+# tmux aliases
+alias tmls='tmux list-sessions'
+alias tma='tmux attach-session'
+alias tmn='tmux new-session'
+alias tmks='tmux kill-server'
+
+# task warrior aliases
+alias t='task'
+alias tw='timew'
