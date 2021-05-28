3d9933d342fa7504810ba43b06a4298854d1756e
Bartek Taczała <bartek@taczala.pl>
2021-05-28 13:46:39 +0200

    Updates

--- installers/macos.sh
+++ installers/macos.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-brew install fd fzf exa gnupg pass tig python3 node bat
+brew install fd fzf exa gnupg pass tig python3 node bat tmuxinator coreutils dfc
+
+# development stuff
+brew install ninja just llvm freetype ccache git-delta
