7c97b3966eead57179aa6f2829c01fcbfd9244a2
Bartek Taczała <bartek@taczala.pl>
2021-09-02 11:31:42 +0200

    installers: Add rg

--- installers/macos.sh
+++ installers/macos.sh
@@ -4,7 +4,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 brew install fd fzf exa gnupg pass tig python3 node bat tmuxinator coreutils dfc htop pinentry-mac timewarrior task
 
 # development stuff
-brew install ninja just llvm freetype ccache git-delta conan
+brew install ninja just llvm freetype ccache git-delta conan rg
 
 # cask
 brew install --cask rectangle spotify
