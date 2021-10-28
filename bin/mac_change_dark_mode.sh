24a419a5093d278f8fea3a65a0e812193af575e6
Bartosz Taczała <btaczala@inmusicbrands.com>
2021-10-28 15:14:06 +0200

    kitty: One only one unix pipe

--- bin/mac_change_dark_mode.sh
+++ bin/mac_change_dark_mode.sh
@@ -2,7 +2,7 @@
 
 mode=$($HOME/dotfiles/bin/darkMode.py)
 if [[ ! ( $($HOME/dotfiles/bin/darkMode) =~ 'Dark' ) ]]; then
-  /usr/local/bin/fd mykitty /tmp -x /usr/local/bin/kitty @ --to unix:{} set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow.conf
+  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow.conf
 else
-  /usr/local/bin/fd mykitty /tmp -x /usr/local/bin/kitty @ --to unix:{} set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow_Night.conf
+  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow_Night.conf
 fi
