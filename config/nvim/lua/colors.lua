49733aea61b0136157f91a06eeb197177a3a647c
Bartosz Taczała <btaczala@inmusicbrands.com>
2021-11-02 15:01:31 +0100

    nvim: Use tokyonight colorscheme

--- config/nvim/lua/colors.lua
+++ config/nvim/lua/colors.lua
@@ -1,49 +1,8 @@
 -- theme change on MacOS
 if vim.fn.has("mac") == 1 then
     local dn = require('dark_notify')
-    dn.run(
-    {
-        onchange = function(mode)
-            if mode == "dark" then
-                require('github-theme').setup({
-                      theme_style = "dark",
-                      transparent = true,
-                      hide_inactive_statusline = false,
-                })
-            else
-                require('github-theme').setup({
-                      theme_style = "light",
-                      transparent = true,
-                      hide_inactive_statusline = false,
-                })
-            end
-        end
-    })
-    dn.update()
+    dn.run()
 end
 
-
-if vim.fn.has("mac") == 1 then
-    local handle = io.popen("$HOME/dotfiles/bin/darkMode")
-    local darkMode = handle:read("*a")
-    if string.find(darkMode, "Dark") then
-        print("Dark mode activated")
-        require('github-theme').setup({
-              theme_style = "dark",
-              transparent = true,
-              hide_inactive_statusline = false,
-        })
-    else
-        print("Dark mode is not on?", darkMode)
-        require('github-theme').setup({
-              theme_style = "light",
-              transparent = true,
-              hide_inactive_statusline = false,
-        })
-    end
-else
-    require('github-theme').setup({
-          theme_style = "light",
-    })
-end
-
+vim.g.tokyonight_transparent = true
+vim.cmd[[colorscheme tokyonight]]
