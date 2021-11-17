2dd7acfc903f0006d83eca940af2acc58dc857d4
Bartosz Taczała <btaczala@inmusicbrands.com>
2021-11-17 15:17:34 +0100

    colors: Use tokyonight dark/day modes on nvim & tmux

--- bin/mac_change_dark_mode.sh
+++ bin/mac_change_dark_mode.sh
@@ -2,7 +2,13 @@
 
 mode=$($HOME/dotfiles/bin/darkMode.py)
 if [[ ! ( $($HOME/dotfiles/bin/darkMode) =~ 'Dark' ) ]]; then
-  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow.conf
+  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured $HOME/dotfiles/config/kitty/kitty_tokyonight_day.conf
 else
-  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured ~/.config/kitty/kitty-themes/themes/Tomorrow_Night.conf
+  /usr/local/bin/kitty @ --to unix:/tmp/mykitty set-colors --all --configured $HOME/dotfiles/config/kitty/kitty_tokyonight_night.conf
 fi
+
+if pgrep -x "tmux" > /dev/null
+then
+    $HOME/dotfiles/tmux/darkmode_tmux.sh
+fi
+
