641a793542f50cd260f69d2636801ca46f4b95a5
Bartosz Taczała <btaczala@inmusicbrands.com>
2022-03-09 12:34:50 +0100

    lsp: Use shellcheck more

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -42,6 +42,8 @@ null_ls.setup({
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.code_actions.shellcheck,
 	},
 	on_attach = on_attach,
 })
